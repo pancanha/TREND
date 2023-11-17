1a97a19a99ea4a6563ff471151a710a808723d60
dioniziojonathan <123213031+pancanha@users.noreply.github.com>
2023-11-17 14:07:13 -0300

    ajeitar o click para pausar a imagem

--- trend/js/slide.js
+++ trend/js/slide.js
@@ -1,37 +1,71 @@
 //------------------------SLIDE--------------------------//
 var radio = document.querySelector('.manual-btn');
 var cont = 1;
-var intervalId;
+var startX;  // Posição inicial do mouse no eixo X
+var imagem = document.getElementById('carouselImage');
+var intervalId;  // ID do intervalo para o setInterval
 
 document.getElementById('radio1').checked = true;
 
-// Iniciar o carrossel
-intervalId = setInterval(() => {
-    proximaImg();
-}, 1000);
+// Adicionar eventos ao documento para mousedown, mousemove e mouseup
+document.addEventListener('mousedown', function(event) {
+    // Ao segurar o clique, armazenar a posição inicial
+    startX = event.clientX;
 
-// Adicionar eventos ao documento para mousedown e mouseup
-document.addEventListener('mousedown', function() {
-    // Ao segurar o clique, pausar o carrossel
+    // Pausar o intervalo ao clicar e segurar a imagem
     clearInterval(intervalId);
 });
 
-document.addEventListener('mouseup', function() {
-    // Ao soltar o clique, retomar o carrossel
-    intervalId = setInterval(() => {
-        proximaImg();
-    }, 1000);
+document.addEventListener('mousemove', function(event) {
+    // Verificar se o botão do mouse está pressionado
+    if (event.buttons === 1) {
+        // Determinar a direção do movimento do mouse
+        var deltaX = event.clientX - startX;
+
+        // Se o movimento for significativo, avançar para a próxima imagem
+        if (Math.abs(deltaX) > 50) {
+            proximaImg(deltaX > 0);
+            startX = event.clientX;  // Atualizar a posição inicial após avançar para a próxima imagem
+        }
+    }
 });
 
-function proximaImg() {
-    cont++;
+function avancarAutomaticamente() {
+    proximaImg(true);
+}
 
-    if (cont > 3) {
-        cont = 1;
+// Iniciar o intervalo quando a página carregar
+intervalId = setInterval(avancarAutomaticamente, 10000);
+
+document.addEventListener('mouseup', function() {
+    // Ao soltar o clique, reiniciar a posição inicial
+    startX = null;
+
+    // Limpar o intervalo antes de configurar um novo
+    clearInterval(intervalId);
+
+    // Retomar o intervalo ao soltar a imagem
+    intervalId = setInterval(avancarAutomaticamente, 10000);
+});
+
+
+
+function proximaImg(avancar) {
+    // Se avancar for true, vá para a próxima imagem; caso contrário, vá para a imagem anterior
+    if (avancar) {
+        cont = cont === 1 ? 3 : cont - 1;
+        
+    } else {
+        cont = cont === 3 ? 1 : cont + 1;
     }
 
     document.getElementById('radio' + cont).checked = true;
 }
+
+// Iniciar o intervalo quando a página carregar
+intervalId = setInterval(function() {
+    proximaImg(true);
+}, 10000);
 //-------------------------- MENU HAMBURGUER-----------------//
 
 function mudouTamanho() {
